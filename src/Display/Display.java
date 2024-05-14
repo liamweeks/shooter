9471698a2513e898e994f894f1a6b382a164f40c
Liam Weeks <liamweeks99@gmail.com>
2024-05-14 16:50:38 -0700

    initial commit

--- src/Display/Display.java
+++ src/Display/Display.java
@@ -1,2 +1,33 @@
-package Display;public class Display {
+package Display;
+import javax.swing.*;
+import java.awt.*;
+
+public class Display extends Canvas {
+    private static final int WIDTH = 800;
+
+    /*
+        private: this code is only viewable inside the Display class
+        static: not an Display instance, but part of the Display type, so we can use it even without instantiating a Display object
+        final: makes a method/variable immutable, so we cannot modify their value even if we inherit or override
+
+     */
+    private static final int HEIGHT = 600;
+    private static final String TITLE = "Shooter Game";
+
+    public static void main(String[] args) {
+
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+
+
+        Display game = new Display();
+        JFrame frame = new JFrame();
+        frame.add(game);
+        frame.pack();
+        frame.setTitle(TITLE);
+        frame.setSize(screenSize);
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE /*3*/);
+        //frame.setSize(WIDTH, HEIGHT);
+        frame.setResizable(false);
+        frame.setVisible(true);
+    }
 }
